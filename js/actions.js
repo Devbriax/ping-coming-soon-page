05e74ee9ceec02b479b2165d6a435beb67b1573f
Brian JL. Mabaso| MBxZO <mbrianjnr02@gmail.com>
2025-02-01 11:58:04 +0200

    resolve javascript onclick butn function for the pop up.
    Signed-off-by: Brian JL. Mabaso| MBxZO <mbrianjnr02@gmail.com>

---
+++ js/actions.js
@@ -0,0 +1,10 @@
+//Form action once the data has been captured
+
+let document.getElementById("email_address").innerHTML = "Thank you for your subscription";
+
+
+//Button action once clicked
+onclick=(function alertClick)(document.getElementById("submit-btn")
+   onclick = (function alertClick() {
+      alert("You've subscribed to our newsletter!");
+   });
